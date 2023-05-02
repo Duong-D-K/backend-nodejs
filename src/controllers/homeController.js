ca877ca4b8811db459d875dcc96d059ecb920f24
Duong-D-K <tuthien1508.2k@gmail.com>
2023-05-02 10:59:31 +0800

    ket thuc bai 27-thuc hien chuc nang edit user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -33,9 +33,34 @@ let postCRUD = async (req, res) => {
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
 
-    console.log(data);
+    // console.log(data);
+
     return res.render("displayCRUD.ejs", {
         dataTable: data,
+    });
+};
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+
+        return res.render("editCRUD.ejs", {
+            user: userData,
+        });
+    } else {
+        return res.send("User not found");
+    }
+};
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+
+    let allUses = await CRUDService.updateUserData(data);
+
+    return res.render("displayCRUD.ejs", {
+        dataTable: allUses,
     });
 };
 
@@ -45,4 +70,6 @@ module.exports = {
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 };
