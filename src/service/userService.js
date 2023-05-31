69e181fcb7257674f034675463993f6d303d6cdf
Duong-D-K <tuthien1508.2k@gmail.com>
2023-05-31 23:41:40 +0800

    59 lesson: debugging redux and execute create new user with redux

--- src/service/userService.js
+++ src/service/userService.js
@@ -110,8 +110,9 @@ let createNewUser = (data) => {
                     lastName: data.lastName,
                     address: data.address,
                     phoneNumber: data.phoneNumber,
-                    gender: data.gender === "1" ? true : false,
-                    roleId: data.roleId,
+                    gender: data.gender,
+                    roleId: data.role,
+                    positionId: data.position,
                 });
 
                 resolve({ errCode: 0, errMessage: "OK" });
